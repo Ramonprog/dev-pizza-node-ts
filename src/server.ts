d576df1d64b9ec649e2a7dc5a1c3246e4cc2b968
Ramonprog <programador.ramon@gmail.com>
2024-03-18 20:39:24 -0300

    feat: tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors"; //ajuda no tratamento de erros
+import cors from "cors";
+
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
+
+//Deixando erros mais "amigáveis"
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+
+  return res
+    .status(500)
+    .json({ status: "error", message: "Internal server error." });
+});
 
 app.listen(3333, () => console.log("Server running at port 3333"));
