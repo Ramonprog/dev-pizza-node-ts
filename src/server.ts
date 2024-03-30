775cdc8ded8dc5ae976269b34d5c5c893452f0ba
Ramonprog <programador.ramon@gmail.com>
2024-03-30 20:53:58 -0300

    feat: exibindo imagem

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors"; //ajuda no tratamento de erros
 import cors from "cors";
+import path from "path";
 
 import { router } from "./routes";
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+
+app.use("/files", express.static(path.resolve(__dirname, "..", "temp"))); // para exibir a imagem da pasta temp
 
 //Deixando erros mais "amigáveis"
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
